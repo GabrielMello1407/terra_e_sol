0faf258a30bd4da096ed7ded89fddd6e3975d499
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-08-02 16:03:33 -0300

    feat: added cors to server  permission

--- api-terra-e-sol/src/server/Server.ts
+++ api-terra-e-sol/src/server/Server.ts
@@ -1,17 +1,26 @@
-// código base do servidor express
-import express, { Request, Response } from "express";
+// src/server/Server.ts
+
+import express from "express";
 import "dotenv/config";
+import cors from "cors"; // Importando o pacote cors
 
 import "./shared/services/TraducoesYup";
-
 import { router } from "./routes";
 
 require("./database");
 
 const server = express();
 
-server.use(express.json());
+// Configuração do CORS
+server.use(
+  cors({
+    origin: "http://localhost:3001", // Permite acesso do seu frontend rodando nesta origem
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+  })
+);
 
+server.use(express.json());
 server.use(router);
 
 export { server };
