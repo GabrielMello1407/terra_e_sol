0b0d978a2ebd0fc6f00e20b991c13c89800a0de1
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-08-07 15:31:21 -0300

    feat: added new routes backend

--- front-end-terra-e-sol/service/PedidoService.tsx
+++ front-end-terra-e-sol/service/PedidoService.tsx
@@ -1,11 +1,36 @@
-import axios from 'axios';
+import { Pedido } from '@/app/types/PedidoTypes';
+import axios, { AxiosResponse } from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:3333',
 });
 
 export class PedidoService {
-  listarTodos() {
-    return axiosInstance.get('/pedidos');
+  // Listar todos os pedidos
+  listarTodos(): Promise<AxiosResponse<Pedido[]>> {
+    return axiosInstance.get<Pedido[]>('/pedidos');
+  }
+
+  // Criar um novo pedido
+  criarPedido(dadosPedido: Omit<Pedido, 'id'>): Promise<AxiosResponse<Pedido>> {
+    return axiosInstance.post<Pedido>('/pedidos', dadosPedido);
+  }
+
+  // Obter um pedido pelo ID
+  obterPorId(id: number): Promise<AxiosResponse<Pedido>> {
+    return axiosInstance.get<Pedido>(`/pedidos/${id}`);
+  }
+
+  // Atualizar um pedido pelo ID
+  atualizarPorId(
+    id: number,
+    dadosAtualizados: Partial<Omit<Pedido, 'id'>>,
+  ): Promise<AxiosResponse<Pedido>> {
+    return axiosInstance.put<Pedido>(`/pedidos/${id}`, dadosAtualizados);
+  }
+
+  // Deletar um pedido pelo ID
+  deletarPorId(id: number): Promise<AxiosResponse<void>> {
+    return axiosInstance.delete<void>(`/pedidos/${id}`);
   }
 }
